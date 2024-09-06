522d0250e4e071fe09df71870b1b29f10aada877
Elias Bosco <elias@engrene.com.br>
2024-09-06 22:41:29 +0100

    JwtStrategy breaking api on validation expired token validation issue fixed

--- src/infrastructure/security/http/jwt.strategy.ts
+++ src/infrastructure/security/http/jwt.strategy.ts
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    const { password, ...result } = await this.userRepository.findById(payload.id);
-    return result;
+    const { password, ...result } = await this.userRepository.findById(payload.id) || {};
+    return Object.keys(result).length ? result : null;
   }
 }
